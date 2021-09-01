cff7af05df38d5408f1124a4c615f6397f2bca48
Benjamin Lizana <benjamin.lizanam@hotmail.com>
2021-08-31 22:37:06 -0400

    Fin sección 8

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -11,8 +11,11 @@ export class SidebarService {
       icon: 'mdi mdi-gauge',
       submenu: [
         { titulo: 'Main', url: '/'},
-        { titulo: 'ProgressBar', url: 'progress'},
-        { titulo: 'Gráficas', url: 'grafica1'},
+        { titulo: 'ProgressBar', url: 'progress' },
+        { titulo: 'Gráficas', url: 'grafica1' },
+        { titulo: 'Promesas', url: 'promesas' },
+        { titulo: 'Rxjs', url: 'rxjs' },
+
       ]
     }
   ];
